de3c8c3db663216626fed3b6cef892d7f5132105
Skprabhat <nsaikrishnaprabhat@gmail.com>
2020-03-02 23:24:08 +0530

    Pause_Menu

---
+++ Fps Survival/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    bool isPauseMenu;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPauseMenu = !isPauseMenu;
+            Time.timeScale = 0;
+        }
+        if (isPauseMenu)
+        {
+            Time.timeScale = 1;
+            pauseMenu.SetActive(true);
+        }
+        if (!isPauseMenu)
+        {
+            pauseMenu.SetActive(false);
+        }
+    }
+}
